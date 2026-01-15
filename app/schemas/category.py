e42d0d8580ea0dc65e15517686121451d4d7ac8e
softlyfear <softlyfear@gmail.com>
2026-01-15 23:28:37 +0300

    fill categories schemas

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -1,5 +1,24 @@
-from pydantic import BaseModel
+from schemas.base import BaseSchema
+from datetime import datetime
 
 
-class CategoryBase(BaseModel):
+class CategoryBase(BaseSchema):
+    name: str
+    slug: str
+    parent_id: int | None = None
+
+
+class CategoryCreate(CategoryBase):
     pass
+
+
+class CategoryUpdate(BaseSchema):
+    name: str | None = None
+    slug: str | None = None
+    parent_id: int | None = None
+
+
+class CategoryRead(CategoryBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
