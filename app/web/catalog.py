e2f3dcacd8fc8b19c7eede13e7bb6bb0bde8eb7a
softlyfear <softlyfear@gmail.com>
2026-01-17 22:40:00 +0300

    add templates config

--- app/web/catalog.py
+++ app/web/catalog.py
@@ -1,16 +1,12 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-
-
-templates = Jinja2Templates(directory="app/templates")
-
+from app.core.templates import templates
 
 router = APIRouter()
 
 
 @router.get("/", response_class=HTMLResponse)
-async def read_home(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="home.html",
