1b8f95cc72c5747e9dacdf5704582b2f474d1b26
softly <softlyfear@gmail.com>
2026-01-17 19:49:01 +0300

    add Jinja2

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from web-shop-fastapi-template!")
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="item.html", context={"id": id}
+    )
