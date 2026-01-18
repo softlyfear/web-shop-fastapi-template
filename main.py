b08ef46ca0a1b8e4cdc825a5b92b72f27a6acbd0
softlyfear <softlyfear@gmail.com>
2026-01-18 13:35:28 +0300

    test pre-commit

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-
-
-
-
-
 from fastapi import FastAPI
-
-from app.web.router import router as web_router
-app = FastAPI()
-
-
 from fastapi.staticfiles import StaticFiles
 
 from app.web.router import router as web_router
 
 app = FastAPI()
-
 
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
