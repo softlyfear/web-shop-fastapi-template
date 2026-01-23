6f33b7c606f08c2c1abf2ec7bc8ecb5d0f6831df
softlyfear <softlyfear@gmail.com>
2026-01-23 23:43:33 +0300

    add autorun migrations

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from contextlib import asynccontextmanager
 
+import uvicorn
+from alembic.config import Config
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
+from alembic import command
 from app.admin.setup import setup_admin
-from app.core import async_engine, settings
-from app.models import Base
+from app.core import settings
 from app.web import router as web_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    pass
     yield
 
 
@@ -26,3 +27,13 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 app.include_router(web_router)
+
+
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+
+if __name__ == "__main__":
+    run_migrations()
+    uvicorn.run("main:app", reload=True)
